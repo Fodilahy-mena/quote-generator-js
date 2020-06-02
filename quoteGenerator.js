2e8a8d5dc1b8fb6012c667fee1ebb9ff0159bf84
JEROME Emilien <jerome.emi@onja.org>
2020-06-02 16:33:12 +0300

    convert the readme with explanation of inside the js quoate file

--- quoteGenerator.js
+++ quoteGenerator.js
@@ -1,1 +1,39 @@
-console.log('it works!');
+//Let ask for an input between 0 and 5, 0 and are uninclusive from user
+
+let numberOfQuoates = Number(prompt("Enter a number between 0 to 5 and you will see what your quoate is:"));
+
+
+//Here is my quoate array and I gave elements as its value.
+
+let quoates = [ 
+                "You can cry again or You can try again. Choice  is yours.",
+                "Success may require a lot of days. But progress only requires one.",
+                "Success is stumbling from failure to failure with no loos of enthusiasm.",
+                "It is easy to fail, but if you do not pay attention and learn from your mistakes, it will never be possible to succeed.", 
+                "Visionary decision-making happens at the intersection of intuition and logic."
+            ];
+
+//Let's call the Math.floor() function to take in a paramiter and round the number downward the nearest number here
+
+let randomNumber = Math.floor(Math.random() * 4);
+function getMyQuoate() {
+    let randomQuoate = Math.floor(Math.random() * (quoates.length));
+    return quoates[randomQuoate];
+}
+
+//Here is the code in which users get the quoate depending on the input they give.
+if (numberOfQuoates === 1 ) {
+    console.log(`${quoates[0]}`);
+} else if (numberOfQuoates === 2 ) {
+    console.log(`${quoates[1]}`);
+} else if (numberOfQuoates === 3) {
+    console.log(`${quoates[2]}`); 
+} else if (numberOfQuoates === 4) {
+    console.log(`${quoates[3]} ${quoates[0]}`);
+} else if (numberOfQuoates === 5) {
+    console.log(`${quoates[4]} ${quoates[1]}`);
+ } else {
+    numberOfQuoates = Number(prompt("Incorrect!, enter a number between 0 to 5 and you will see what your quoate is:"));
+}
+
+
