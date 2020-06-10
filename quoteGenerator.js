5dc336cb4494a7636fad7ffd710f33bed825781f
JEROME Emilien <jerome.emi@onja.org>
2020-06-10 09:53:59 +0300

    almost there

--- quoteGenerator.js
+++ quoteGenerator.js
@@ -1,42 +1,7 @@
-// //Let ask for an input between 0 and 5, 0 and are uninclusive from user
-
-// let numberOfQuoates = Number(prompt("Enter a number between 0 to 5 and you will see what your quoate is:"));
-
-
-// //Here is my quoate array and I gave elements as its value.
-
-// let quoates = [ 
-//                 "You can cry again or You can try again. Choice  is yours.",
-//                 "Success may require a lot of days. But progress only requires one.",
-//                 "Success is stumbling from failure to failure with no loos of enthusiasm.",
-//                 "It is easy to fail, but if you do not pay attention and learn from your mistakes, it will never be possible to succeed.", 
-//                 "Visionary decision-making happens at the intersection of intuition and logic."
-//             ];
-
-// //Let's call the Math.floor() function to take in a paramiter and round the number downward the nearest number here
-
-// let randomNumber = Math.floor(Math.random() * 4);
-// function getMyQuoate() {
-//     let randomQuoate = Math.floor(Math.random() * (quoates.length));
-//     return quoates[randomQuoate];
-// }
-
-// //Here is the code in which users get the quoate depending on the input they give.
-// if (numberOfQuoates === 1 ) {
-//     console.log(`${quoates[0]}`);
-// } else if (numberOfQuoates === 2 ) {
-//     console.log(`${quoates[1]}`);
-// } else if (numberOfQuoates === 3) {
-//     console.log(`${quoates[2]}`); 
-// } else if (numberOfQuoates === 4) {
-//     console.log(`${quoates[3]} ${quoates[0]}`);
-// } else if (numberOfQuoates === 5) {
-//     console.log(`${quoates[4]} ${quoates[1]}`);
-//  } else {
-//     numberOfQuoates = Number(prompt("Incorrect!, enter a number between 0 to 5 and you will see what your quoate is:"));
-// }
 
 //// Part1
+
+// First set of quote
 
 let array1 = ["You", "maybe", "many", "becomes"];
 let array2 = ["Really", "But", "everything", "suppose"];
@@ -56,6 +21,49 @@ return myQuote;
 }
 //// Part2
 
+// Part 2 of old set of quote
+
+// let numberOfQuotes = Number(prompt("Enter a number of time you want to generate a random quoate:"));
+// if (numberOfQuotes >= 1 && numberOfQuotes <= 5) {
+//     for (let i = 0; i < numberOfQuotes; i++) {
+//         console.log(generateQuote());
+//     } 
+// } else {
+//         console.error('Only enter a number between 1 and 5:');// To check if the number is between 1 and 5
+//     }
+
+    /// Part 3
+
+// Second set of quote
+
+ let otherArray1 = ["True", "according to", "can you", "Robert"];
+ let otherArray2 = ["It depends", "On the beach", "on they went", "supposingly"];
+ let otherArray3 = ["salad", "yammy", "cool", "movies"];
+
+ const generateNewQuote = () => {
+ let randomIndexOther1 = Math.floor(Math.random() * otherArray1.length);
+ let randomIndexOther2 = Math.floor(Math.random() * otherArray2.length);
+ let randomIndexOther3 = Math.floor(Math.random() * otherArray3.length);
+
+ let stringOther1 = otherArray1[randomIndexOther1];
+ let stringOther2 = otherArray2[randomIndexOther2];
+ let stringOther3 = otherArray3[randomIndexOther3];
+
+ let newQuote = `${stringOther1} ${stringOther2} ${stringOther3}`;
+ return newQuote;
+ }
+/// Part2 
+
+//Part 2 of second New set of quote
+
+//  let numberOfQuotes = Number(prompt("Enter a number of time you want to generate a random quoate:"));
+//  if (numberOfQuotes >= 1 && numberOfQuotes <= 5) {
+//      for (let i = 0; i < numberOfQuotes; i++) {
+//          console.log(generateNewQuote());
+//      } 
+//  } else {
+//          console.error('Only enter a number between 1 and 5:');// To check if the number is between 1 and 5
+//      }
 
 //1. use prompt to ask the user how many times we want to generate a random quote
 //2. check if the prompt give us something between 1 and 5
@@ -63,58 +71,30 @@ return myQuote;
 //4. refactor our code into a function, and put the function call inside the loop
 //4.1 generate random numbers
 //4.2 show the generated quotes
-//let firstQuote = generateQuote;
-//let secondQuote = generateNewQuote;
-// let numberOfQuotes = Number(prompt("Enter a number of time you want to generate a random quoate:"));//I've added prompt here
-// if (numberOfQuotes >= 1 && numberOfQuotes <= 5) { // and I use an if statement
-//     for (let i = 0; i < numberOfQuotes; i++) {
-//         //console.log(generateQuote());
-//       numberOfSets = Number(prompt("Enter a number 1 or 2 to help you which quote of set are you looking for:"));
-//       if (numberOfSets === 1) {
-//         console.log(myQuote);
-//       } else if (numberOfSets === 2) {
-//           console.log(newQuote);
-//       } else {
-//           console.error("Only enter a number 1 or 2");
-//       }
-//     }
-// } else {
-//     console.error('Only enter a number between 1 and 5:');// To check if the number is between 1 and 5
-// }
-
-/// Part 3
 
 
-let numberOfSets = Number(prompt("Enter a number 1 or 2 to help you which type of quote set are you looking for:"));//I've added prompt here
-while (numberOfSets === 1 && numberOfSets === 2) {
-    numberOfQuotes = Number(prompt("Enter a number of time you want to generate a random quoate:"));
-    if (numberOfSets === 1 && numberOfQuotes >= 1 && numberOfQuotes <= 5) {
-        for (let i = 0; i < numberOfQuotes; i++) {
-            console.log(generateQuote);
-        }
-    } else if (numberOfSets === 2 && numberOfQuotes >= 1 && numberOfQuotes <= 5) {
-        for (let i = 0; i < numberOfQuotes; i++) {
-            console.log(generateQuote);
-        }
-    } else {
-        console.error('Only enter a number between 1 and 5:');
+let numberOfSets = Number(prompt("Enter a number 1 or 2 to help you which quote of set are you looking for:"));
+if (numberOfSets === 1) {
+    let numberOfQuotes = Number(prompt("Enter a number of time you want to generate a random quoate:"));
+if (numberOfQuotes >= 1 && numberOfQuotes <= 5) {
+    for (let i = 0; i < numberOfQuotes; i++) {
+        console.log(generateQuote());
+    } 
+} else {
+        console.error('Only enter a number between 1 and 5:');// To check if the number is between 1 and 5
     }
-} 
-
-
-let otherArray1 = ["True", "according to", "can you", "Robert"];
-let otherArray2 = ["It depends", "On the beach", "on they went", "supposingly"];
-let otherArray3 = ["salad", "yammy", "cool", "movies"];
-
-const generateNewQuote = () => {
-let randomIndexOther1 = Math.floor(Math.random() * otherArray1.length);
-let randomIndexOther2 = Math.floor(Math.random() * otherArray2.length);
-let randomIndexOther3 = Math.floor(Math.random() * otherArray3.length);
-
-let stringOther1 = otherArray1[randomIndexOther1];
-let stringOther2 = otherArray2[randomIndexOther2];
-let stringOther3 = otherArray3[randomIndexOther3];
-
-let newQuote = `${stringOther1} ${stringOther2} ${stringOther3}`;
-return newQuote;
+} else if (numberOfSets === 2) {
+    let numberOfQuotes = Number(prompt("Enter a number of time you want to generate a random quoate:"));
+if (numberOfQuotes >= 1 && numberOfQuotes <= 5) {
+    for (let i = 0; i < numberOfQuotes; i++) {
+        console.log((generateNewQuote));
+    } 
+} else {
+        console.error('Only enter a number between 1 and 5:');// To check if the number is between 1 and 5
+    }
+} else {
+    console.error('Only enter a number 1 or 2:');
 }
+
+
+
